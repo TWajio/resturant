e7b432d99a228f6dc9fdba3ca820c2db31f567be
Wajio Tausif <mdwajio@gmail.com>
2025-09-06 16:10:22 +0600

    Updates

--- assets/cart-CdGlw_Z4.js
+++ assets/cart-CdGlw_Z4.js
@@ -1,4 +1,4 @@
-import"./main-DWuNqzL1.js";document.addEventListener("DOMContentLoaded",()=>{let a=JSON.parse(localStorage.getItem("cart"))||[];const e=document.getElementById("cart-page-items"),n=document.getElementById("cart-page-subtotal");function s(){e.innerHTML="";let o=0;if(a.length===0){e.innerHTML='<p class="text-gray-500">Your cart is empty.</p>',n.textContent="0tk";return}a.forEach(t=>{const r=t.price*t.qty;o+=r,e.innerHTML+=`
+import"./main-DRS4zpMZ.js";document.addEventListener("DOMContentLoaded",()=>{let a=JSON.parse(localStorage.getItem("cart"))||[];const e=document.getElementById("cart-page-items"),n=document.getElementById("cart-page-subtotal");function s(){e.innerHTML="";let o=0;if(a.length===0){e.innerHTML='<p class="text-gray-500">Your cart is empty.</p>',n.textContent="0tk";return}a.forEach(t=>{const r=t.price*t.qty;o+=r,e.innerHTML+=`
         <div class="flex items-center gap-4 border-b pb-4">
           <img src="${t.image}" alt="${t.name}" 
                class="w-20 h-20 object-cover rounded-md shadow" />
